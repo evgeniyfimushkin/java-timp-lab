22128867cbe694b196ee954cc6cf9abc51ecb21b
Evgeniy Fimushkin <fimnames@gmail.com>
2024-03-03 12:58:06 +0700

    Habitat implemented

--- src/main/java/edu/evgen/habitat/HabitatImpl.java
+++ src/main/java/edu/evgen/habitat/HabitatImpl.java
@@ -5,23 +5,48 @@ import edu.evgen.habitat.employee.IBehaviour;
 import edu.evgen.habitat.employee.Manager;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @Data
 @RequiredArgsConstructor
+@Slf4j
 public class HabitatImpl implements Habitat {
     final Long developerDelay;
     final Long managerDelay;
     final Double developerProbability;
     final Double managerRatio;
-    final Set<Developer> developers = new HashSet<>();
-    final Set<Manager> managers = new HashSet<>();
+    final List<Developer> developers = new ArrayList<>();
+    final List<Manager> managers = new ArrayList<>();
+    final Random random = new Random();
+    final Long paneSize = 500L;
     @Override
     public Optional<IBehaviour> birthAttempt() {
-        return Optional.empty();
+        log.info("birthAttempt <-");
+        LocalDateTime now = LocalDateTime.now();
+
+        if (
+                (developers.isEmpty() || developers.getLast().getBirthTime().plusSeconds(developerDelay).isBefore(now)) &&
+                        (random.nextDouble() <= developerProbability)
+        ) {
+            log.info("Developer birth!");
+            developers.add(new Developer(developerDelay, developerProbability, paneSize));
+            return Optional.of(developers.getLast());
+
+        }
+
+        if (
+                (managers.isEmpty() || managers.getLast().getBirthTime().plusSeconds(managerDelay).isBefore(now)) &&
+                        ((double)managers.size()/Math.max(developers.size(),1) < managerRatio)
+        ) {
+            log.info("Manager birth!");
+            managers.add(new Manager(managerDelay, paneSize));
+            return Optional.of(developers.getLast());
+
+        }
+            return Optional.empty();
     }
 
     @Override
--- src/main/java/edu/evgen/habitat/employee/Employee.java
+++ src/main/java/edu/evgen/habitat/employee/Employee.java
@@ -19,6 +19,7 @@ public abstract class Employee implements IBehaviour {
     protected final Random random = new Random();
     protected final ImageView imageView;
     protected final Long paneSize;
+    protected final LocalDateTime birthTime = LocalDateTime.now();
 
     public Optional<IBehaviour> birthAttempt(){
         return Optional.empty();
