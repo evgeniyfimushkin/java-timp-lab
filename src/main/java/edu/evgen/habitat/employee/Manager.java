23912cc7e372de214c82ab29d88f600a2538172b
Evgeniy Fimushkin <fimnames@gmail.com>
2024-03-19 16:14:06 +0700

    Manager direction added

--- src/main/java/edu/evgen/habitat/employee/Manager.java
+++ src/main/java/edu/evgen/habitat/employee/Manager.java
@@ -8,8 +8,10 @@ import static java.lang.Math.sin;
 public class Manager extends Employee {
     private final Integer anchorRadius = 80;
     private Double alfa = 0.0;
+    private final Double speed = 0.01 * (random.nextDouble()+0.3);
     private final Double anchorPointX;
     private final Double anchorPointY;
+    Integer direction = random.nextBoolean() ? 1 : -1;
     public Manager(Long paneSize, Long livingTime) {
         super("/manager.png", paneSize, livingTime);
         this.anchorPointX = initAnchorPoint(x);
@@ -22,7 +24,7 @@ public class Manager extends Employee {
     }
     @Override
     public void move() {
-            alfa += 0.01;
+            alfa += direction*speed;
             move(
                     anchorPointX + anchorRadius * cos(alfa),
                     anchorPointY + anchorRadius * sin(alfa));
@@ -35,8 +37,12 @@ public class Manager extends Employee {
     protected void move(Double x, Double y) {
         if ((x < paneSize) && (x > 0))
             this.x = x;
+        else
+            direction = -direction;
         if ((y < paneSize) && (y > 0))
             this.y = y;
+        else
+            direction = -direction;
         Platform.runLater(() -> {
             imageView.setX(this.x);
             imageView.setY(this.y);
