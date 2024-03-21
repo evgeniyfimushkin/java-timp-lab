73b82acccd2ea71029dcdef0ed8c3ad9de876bff
Evgeniy Fimushkin <fimnames@gmail.com>
2024-03-21 13:00:36 +0700

    Network Ui + Server and Client introduced

---
+++ src/main/java/edu/evgen/Client.java
@@ -0,0 +1,23 @@
+package edu.evgen;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Client {
+    public static final Integer PORT = 19000;
+    public static final String HOST = "localhost";
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = null;
+
+        try {
+            socket = new Socket(HOST, PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            socket.close();
+        }
+    }
+}
